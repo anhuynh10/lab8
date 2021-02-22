2077de3e1937e353f09ee85353fa76c245b2c467
An Huynh <anh005@ucsd.edu>
2021-02-22 15:16:00 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
-	// add your code here
+  // add your code here
+  L.mapquest.key = "KEY";
+
+  // 'map' refers to a <div> element with the ID map
+  var map = L.mapquest.map('map', {
+	center: [32.88, -117.236],
+	layers: L.mapquest.tileLayer('map'),
+	zoom: 12,
+	zoomControl: false
+  });
+
+  L.marker([32.878626,-117.235895]).addTo(map);
 }
+
